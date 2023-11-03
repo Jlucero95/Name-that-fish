67aa678778c88cb357d0652c11e98959d820656a
Jeremy Lucero <Lucero.jeremy07@gmail.com>
2023-11-03 18:28:10 -0500

    Class component side is fully functional
    Time for code clean up.

--- src/Components/Class/ClassScoreBoard.tsx
+++ src/Components/Class/ClassScoreBoard.tsx
@@ -2,28 +2,15 @@ import { Component } from "react";
 import "./styles/score-board.css";
 import { TGuessCount } from "./ClassApp";
 
-const answersLeft = ["trout", "salmon", "tuna", "shark"];
+const initialAnswersLeft = ["trout", "salmon", "tuna", "shark"];
 
 export class ClassScoreBoard extends Component<{
 	countNumbers: TGuessCount;
 }> {
-	state = {
-		countCheck: 0,
-		countTest: false,
-	};
-
 	render() {
 		const { countNumbers } = this.props;
-		const { countCheck, countTest } = this.state;
 		const totalCount = countNumbers.correctCount + countNumbers.incorrectCount;
-		if (totalCount > countCheck) {
-			this.setState({ countCheck: countCheck + 1 });
-			this.setState({ countTest: true });
-		}
-		if (countTest) {
-			answersLeft.splice(answersLeft.length - 1);
-			this.setState({ countTest: false });
-		}
+		const answersLeft = initialAnswersLeft.slice(totalCount);
 		return (
 			<div id="score-board">
 				<div>Incorrect 🔻: {countNumbers.incorrectCount}</div>
